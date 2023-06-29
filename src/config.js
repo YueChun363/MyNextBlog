0ea0f6d6d1c87c9451db3c6bceccc528af3817f0
yuechun <1013582515@qq.com>
2023-06-29 14:45:31 +0800

    new changes

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const config = {
+  title: " InnozzZ",
+  description: "分享 JavaScript 以及热门框架，记录前端工程师学习成长历程。",
+  author: "InnozzZ",
+  avatar:
+    "https://p3-passport.byteimg.com/img/user-avatar/d6a270e545b3a8feee4556b8b2ddfa82~100x100.awebp",
+  banner: "InnozzZ Blog",
+};
